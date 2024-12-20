b8665b2a66f2195e9683996cdc6ca20900aceb50
111360136 <t111360136@ntut.org.tw>
2024-12-20 15:43:21 +0800

    lab12

---
+++ app/src/main/java/com/example/lab12/Data.java
@@ -0,0 +1,14 @@
+package com.example.lab12;
+
+public class Data {
+    Result result;
+
+    static class Result{
+        Result[] results;
+
+        static class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
